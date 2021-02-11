b4974766ce668fcc110fa2209101a569ff5036b3
Matthew Justice <matthewajustice@gmail.com>
2021-02-11 13:05:02 -0500

    Initial api commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
-console.log('Server is up and running good buddy');
-// console.log(__dirname);
-// console.log(__filename);
-console.log(process.env.USERNAME);
-console.log(process.env.PORT);
-console.log(process.env.GREET);
-console.log(process.env.FOOD);
-console.log(process.env.REALLY);
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/*', (_, res) => {
+  res.json({ data: 'The API is working' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port: ${port}`);
+});
