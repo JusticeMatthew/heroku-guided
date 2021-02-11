352b2e80e92956eb4684ff8c011b4e8a5d870d8e
Matthew Justice <matthewajustice@gmail.com>
2021-02-11 13:50:17 -0500

    Monolith push

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+    ? `http://localhost:5000${path}`
+    : path;
+}
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch(url('/api/'))
+      .then((res) => res.json())
+      .then((apiData) => setData(apiData.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>API Data: {data}</header>
+    </div>
+  );
+}
+
+export default App;
