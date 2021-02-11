87a98c6028ec4f704a66980cc71b97a21b63f726
Matthew Justice <matthewajustice@gmail.com>
2021-02-11 12:44:04 -0500

    Renaming repo

---
+++ index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv').config();
+
+console.log('Server is up and running good buddy');
+// console.log(__dirname);
+// console.log(__filename);
+console.log(process.env.USERNAME);
+console.log(process.env.PORT);
+console.log(process.env.GREET);
+console.log(process.env.FOOD);
+console.log(process.env.REALLY);
